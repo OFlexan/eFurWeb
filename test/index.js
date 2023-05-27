a5f50e62ff92dd95f6600188ca98389b4eb9f8a0
OFlexan <pieterdenbreeyen@gmail.com>
2023-05-27 19:29:57 +0200

    i leaked some of my testing stuff :(

--- test/index.js
+++
@@ -1,9 +0,0 @@
-nav.setContainer(document.querySelector("#test"));
-nav.start();
-
-nav.listen("index", function() {
-  nav.add("p", {
-    class: "test",
-    $innerText: "Hello world!"
-  });
-});
